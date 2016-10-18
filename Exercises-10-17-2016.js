f2af6ef946962322a531899141c9840454b95d78
Jodi DeGrave <jdegrave@earthlink.net>
2016-10-17 20:57:06 -0500

    update

---
+++ Exercises-10-17-2016.js
@@ -0,0 +1,12 @@
+/* Conditionals Exercise 10/17/2016 Monday Lecture - Brenna
+
+/* Write a conditional that evaluates a string.
+ * If the string is your name, log a custom greeting
+ * If the string isn't your name, log a generic greeting.
+*/
+ var myString = 'Jodi';
+
+ if (myString === 'Jodi'){
+    console.log('Hello, ' + myString + '!');
+ } else
+    console.log('Greetings, Earthling!');
