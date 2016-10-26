e8f464bfd7c7238c25b964dd46f85b32fc3840af
Jodi <jdegrave@earthlink.net>
2016-10-26 12:13:40 -0500

    Added constructor with 'this' - works!

--- Homework/Homework-03/exercism/javascript/leap/leap.js
+++ Homework/Homework-03/exercism/javascript/leap/leap.js
@@ -1,17 +1,22 @@
-var Year = function() {};
+//var Year = function() {};
 
-Year.prototype.isLeap = function(year) {
-    
+function Year(year) {
+    this.year = year;
+}
+
+
+Year.prototype.isLeap = function() {
+
   // If year is evenly divisible by 4 and not evenly disibly by 100 but is evenly divisible by 400, it's a leap year)    
-  if (year % 400 === 0) {
+  if (this.year % 400 === 0) {
         return true;
-  } else if (year % 100 === 0) {
+  } else if (this.year % 100 === 0) {
         return false;
-  } else if (year % 4 === 0) {
+  } else if (this.year % 4 === 0) {
         return true; 
   } else {
         return false;
-  }      
+  }         
 };
 
 module.exports = Year;
