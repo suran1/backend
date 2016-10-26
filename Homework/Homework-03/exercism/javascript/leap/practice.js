aa20c20347614f8385de92d20cf5cb60c78e5b94
Jodi <jdegrave@earthlink.net>
2016-10-25 20:45:43 -0500

    exercism leap plus practice.js to show code

---
+++ Homework/Homework-03/exercism/javascript/leap/practice.js
@@ -0,0 +1,18 @@
+// running code without jasmine-node and guess what? It works!
+
+function isLeap (year) { 
+if (year % 400 === 0) 
+        return true;
+    
+    else if (year % 100 === 0) 
+        return false;
+    
+    else if (year % 4 === 0) 
+        return true; 
+    
+    else
+        return false;
+    
+}
+
+console.log(isLeap(2015));
