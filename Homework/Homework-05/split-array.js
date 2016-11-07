a7300874cf2da49e7486beb7b001cd3a38ef96da
Jodi <jdegrave@earthlink.net>
2016-11-06 23:08:16 -0600

    Added a more correct, but boring function to solve this

--- Homework/Homework-05/split-array.js
+++ Homework/Homework-05/split-array.js
@@ -17,7 +17,26 @@ function splitArray (arr, size) {
   return newArr;
 }    
 
+
 console.log(splitArray(['a', 'b', 'c', 'd'], 2));
 console.log(splitArray(['a', 'b', 'c', 'd', 'e'], 3));
 console.log(splitArray(['a', 'b', 'c', 'd', 'e'], 4));
 console.log(splitArray(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'], 6));
+
+function splitArray2(arr, size) {
+    
+    var newArr = [];
+    
+    newArr.push(arr.slice(0, size));
+    newArr.push(arr.slice(size));
+    
+    return newArr;
+    
+}
+
+console.log('\nThe less fun, but more correct way\:\n');
+
+console.log(splitArray2(['a', 'b', 'c', 'd'], 2));
+console.log(splitArray2(['a', 'b', 'c', 'd', 'e'], 3));
+console.log(splitArray2(['a', 'b', 'c', 'd', 'e'], 4));
+console.log(splitArray2(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'], 6));                    
