b29f2ee2a91962a563866865a9ff93f1fdc62607
Jodi <jdegrave@earthlink.net>
2016-11-13 13:23:02 -0600

    this is target-sum exercise - minor changes

--- Lecture-Code-Examples/alter-array.js
+++ Lecture-Code-Examples/alter-array.js
@@ -5,30 +5,26 @@
 */ 
 
 
-function removeTargetSum (arr, target) {
-    
-    var i = 0;
-    var removed = false;
-    
-    while (i < arr.length - 1) {
-        if (arr[i] + arr[i + 1] === target) {
-            arr.splice(i+1, 1);
-            removed = true;
-        }
-        
-        if (!removed) {
-            i++;
-        }
-        
-        removed = false;
-    } // end while
-    
-    return arr;
-} // end function
-    
+function removeTargetSum(nums, target) {
+  var i = 0;
+  var removed = false;
 
+  while (i < nums.length - 1) {
+    if (nums[i] + nums[i + 1] === target) {
+      nums.splice(i + 1, 1);
+      removed = true;
+    }
 
+    if (!removed) {
+      i++;
+    }
+
+    removed = false;
+  }
+
+  return nums;
+}
 
 console.log(removeTargetSum([1, 3, 5, 6, 7, 4, 3], 7)); // [1, 3, 5, 6, 7, 4]
-//console.log(removeTargetSum([4, 1, 1, 1, 4], 2)); // [4, 1, 4]
-//console.log(removeTargetSum([2, 2, 2, 2, 2, 2], 4)); // [2]
+console.log(removeTargetSum([4, 1, 1, 1, 4], 2)); // [4, 1, 4]
+console.log(removeTargetSum([2, 2, 2, 2, 2, 2], 4)); // [2]
