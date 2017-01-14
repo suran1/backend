d0fb2afb5596312902993e7d0e735abdc91be473
Jodi <jdegrave@earthlink.net>
2017-01-14 14:03:18 -0600

    typo corrections

---
+++ Homework/Homework-12/Q1-Chain-Composition/api.js
@@ -0,0 +1,19 @@
+// Update your api.js file to include:
+
+module.exports = {
+  getName: function (cb) {
+    setTimeout(function () {
+      cb(null, 'Fluffykins');
+    }, 500);
+  },
+  getBreed: function (name, cb) {
+    setTimeout(function () {
+      cb(null, name + ' is a Dalmatian');
+    }, 400);
+  },
+  getCoatColor: function (breed, cb) {
+    setTimeout(function () {
+      cb(null, breed + ' with spots!');
+    }, 550);
+  }
+};
---
+++ Homework/Homework-12/Q1-Chain-Composition/index.js
@@ -0,0 +1,72 @@
+/*
+Create a new file
+  In this file:
+      - Create a function that chains the API functions getName, getBreed and getCoatColor together
+        to print a single phrase (Fluffykins is a Dalmatian with spots!)
+      - Commit & push this file and any changes to GitHub
+*/
+
+var animalInfo = require('./api');
+
+function requestName () {
+  return new Promise(function (resolve, reject){
+    animalInfo.getName(function (err, name){
+      if (err) {
+        reject(err);
+      }
+      resolve(name);
+
+      // remove resolve(name) and just have 'reject' to test err condition
+    });
+  });
+}
+
+function requestBreed () {
+  return new Promise(function (resolve, reject){
+    animalInfo.getBreed(function (err, breed){
+    if (err){
+      reject(err);
+    }
+    resolve(breed);
+    });
+  });
+}
+
+
+function requestCoatColor () {
+  return new Promise(function (resolve, reject){
+    animalInfo.getCoatColor(function (err, coatColor){
+      if (err) {
+        reject(err);
+      }
+      resolve(coatColor);
+    });
+  });
+}
+
+requestName()
+.then(function (nameStr) {
+  console.log(nameStr);
+  return requestBreed(nameStr);
+}).then(function(breed){
+  console.log(breed);
+  return requestCoatColor(breed);
+}).then(function(coatColor){
+  console.log(coatColor);
+}).catch(function(err) {
+  console.log(err);
+});
+
+
+
+
+
+
+// createFile('my-file2.txt', 'Create a file and text')
+// .then(function(str){
+//   console.log(str);
+//   return appendFile('my-file2.txt', 'another line of text');
+// })
+// .then(function(str){
+//   console.log(str);
+// });
