8e559ca9b9f8a1af61c7c066db778f1d70440e57
Jodi DeGrave <jdegrave@earthlink.net>
2017-02-17 19:27:10 -0600

    added comments and additional example

--- Homework/homework-18/question-04/index.js
+++ Homework/homework-18/question-04/index.js
@@ -4,22 +4,27 @@
 
 var events = require('events');
 
-// class that divides two numbers and emits events
+// class checks for 'Q' or 'q' as the first character and emits events
 var QCharEvent = function () {
     events.EventEmitter.call(this);
 }
 
 require('util').inherits(QCharEvent, events.EventEmitter);
 
+// adding the isQChar function to QCharEvent prototype. Any
+// new objects created from QCharEvent inherit this isQChar function
 QCharEvent.prototype.isQChar = function (str){
     var firstChar = str.substr(0,1);
-    var answer = 17;
+    var answer;
 
     if (firstChar == '' || firstChar.toUpperCase() !== 'Q') {
         var error = new Error ('First character isn\'t "Q" or "q".');
         this.emit('error', error);   // attach error to the event
     } else {
-        this.emit('isQChar', answer);   // division is the event name
+        // emit the isQChar event if no error, and send function
+        this.emit('isQChar', answer = 17);   // isQChar is the event name; a bit
+                                            // confusing because is also name of
+                                            // the function 
     }
 
     return this;
@@ -28,14 +33,19 @@ QCharEvent.prototype.isQChar = function (str){
 
 var qcharEvent = new QCharEvent();
 
+// listen for error event; if an error occurs, print error to console
 qcharEvent.on('error', function (error){
     console.log('ERROR: ');
     console.log(error);
 })
 
+//listen for isQChar event (which is any event that isn't an error in this
+// case). callback function takes the result of the this.emit('isQChar', answer)
+// code. In this case the answer is a literal value (17). Result is 1
 qcharEvent.on('isQChar', function (result){
     console.log('RESULT: ', result);
 })
 
 qcharEvent.isQChar('Quail');
 qcharEvent.isQChar('conquer');
+qcharEvent.isQChar('quit');
